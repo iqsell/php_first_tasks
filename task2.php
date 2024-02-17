5f7668940df4599f01f25443d255e6d925265733
iqsell <91834688+iqsell@users.noreply.github.com>
2024-02-17 10:49:51 +0300

    Update task2.php

--- task2.php
+++ task2.php
@@ -2,13 +2,13 @@
 $first_color = readline("Введите первый цвет: ");
 $second_color = readline("Введите второй цвет ");
 
-if ($first_color == 'красный' and $second_color == 'синий') {
+if ($first_color === 'красный' and $second_color === 'синий') {
     echo 'фиолетовый';
-} elseif ($first_color == 'красный' and $second_color == 'желтый') {
+} elseif ($first_color === 'красный' and $second_color === 'желтый') {
     echo 'оранжевый';
-} elseif ($first_color == 'синий' and $second_color == 'желтый') {
+} elseif ($first_color === 'синий' and $second_color === 'желтый') {
     echo 'зеленый';
-} elseif ($first_color == $second_color) {
+} elseif ($first_color === $second_color) {
     echo $first_color;
 } else {
     echo 'Неверный цвет';
