5ae4a0b9f95237bd06bdb939a63e1b4c9581489f
iqsell <91834688+iqsell@users.noreply.github.com>
2024-02-17 10:33:28 +0300

    fix first task

--- test.php
+++ test.php
@@ -9,10 +9,11 @@ if ($sign == '+') {
     echo $a - $b;
 } elseif ($sign == '*') {
     echo $a * $b;
-} elseif ($sign == '/') {
-    echo $a / $b;
 } elseif ($sign == '/' and $b == 0) {
     echo 'На ноль делить нельзя';
+} elseif ($sign == '/') {
+
+    echo $a / $b;
 } else {
     echo 'Неверная операция';
 }
